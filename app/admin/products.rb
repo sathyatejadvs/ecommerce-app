f51e85cbf11ae456f5d63b464e06d0b10b665f2f
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-07 16:48:35 +0530

    Removed Default sidebar on index page

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -4,10 +4,8 @@ ActiveAdmin.register Product do
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   permit_params :title, :description, :price, :main_image, :thumb_image
-  #
-  # or
- 
-  #
+  
+  config.filters = false
   # permit_params do
   #   permitted = [:permitted, :attributes]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?

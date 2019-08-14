d9c5a1e8e7ae718237bc0351dadde8de2f376550
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-14 13:01:30 +0530

    Configured routing to products index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :products
+  get 'products/about'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: "products#index"
 end

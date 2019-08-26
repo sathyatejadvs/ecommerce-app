a3734451085ce9c258f65d484e1e97f692d64150
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-26 12:04:28 +0530

    Added actions to the address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,46 @@
 class AddressesController < InheritedResources::Base
+  before_action :set_address, only: [:show, :edit, :update, :destroy]
+
+  def index
+	@addresses = current_user.addresses
+  end
+
+  # GET /addresses/1
+  # GET /addresses/1.json
+  def show
+  end
+
+  # GET /addresses/new
+  def new
+  	@address = Address.new
+  end
+
+  # GET /addresses/1/edit
+  def edit
+  end
+
+  def create
+  	@address = current_user.addresses.new(address_params)
+
+  	respond_to do |format|
+  		if @address.save
+  			format.html { redirect_to @address, notice: 'address was successfully created.' }
+  			format.json { render :show, status: :created, location: @address }
+  		else
+  			format.html { render :new }
+  			format.json { render json: @address.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
 
   private
 
-    def address_params
-      params.require(:address).permit(:name, :phone, :email, :address_line1, :address_line2)
-    end
+  def set_address
+  @address = current_user.addresses.find(params[:id])
+  end
+
+  def address_params
+  	params.require(:address).permit(:name, :phone, :email, :address_line1, :address_line2)
+  end
 
 end

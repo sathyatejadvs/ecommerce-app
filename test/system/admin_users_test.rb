8c8610e54484d5e70b28243ec9ffd2da1843ab17
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-27 17:58:18 +0530

    Refactored unused test code in admin system testing

--- test/system/admin_users_test.rb
+++ test/system/admin_users_test.rb
@@ -1,11 +1,6 @@
 require "application_system_test_case"
 
 class AdminUsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit admin_users_url
-  #
-  #   assert_selector "h1", text: "AdminUsers"
-  # end
 
    test "should be new user" do
 	   visit new_admin_user_session_url

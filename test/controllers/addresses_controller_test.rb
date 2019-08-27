ce90772f77b1495d8289c94bca7f3da7a19ae052
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-08-27 15:20:12 +0530

    Included test to create address

--- test/controllers/addresses_controller_test.rb
+++ test/controllers/addresses_controller_test.rb
@@ -7,9 +7,16 @@ class AddressesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create address" do
+    assert_difference('Address.count') do
+      post addresses_url, params: { address: { address_line1: "address1", address_line2: "address2", email: "address@gmail.com", name: "satya", phone: "9393838482" } }
+    end
+
+    assert_redirected_to address_url(Address.last)
+  end
+
   test "should get show" do 
     get addresses_url(@address)
     assert_response :success
   end
-  
 end

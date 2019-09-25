91d4705610cd0ccbd6f3968008212298524b9e89
Sathyatejadvs <daggubatisatyateja@gmail.com>
2019-09-25 13:32:00 +0530

    Added after_validation callback
    - Tested the output performing by after_validation method in rails console

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,12 +13,22 @@ class Product
   validates :title, :description, presence: true
   validates :title, uniqueness: true
   validates :price, format: { with:/\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
-
+  
   before_validation :before_validation_method
-
+  after_validation :after_validation_method
 
   def before_validation_method
     puts "Invoked before validation"
     puts self.title = title.upcase
    end
+
+   def after_validation_method
+    if self.errors.any?
+      puts "Validation failed when creating a product"
+      puts self.errors.messages
+    else 
+      puts "Invoked after_validation method"
+    end
+   end
+
 end
